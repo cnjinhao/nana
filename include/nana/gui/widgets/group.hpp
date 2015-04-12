deeb12243bc02184f75d5b8fd28f020fb8a9373a
qPCR4vir <arielvina@yahoo.es>
2015-04-12 23:45:02 +0200

    add empty group files

---
+++ include/nana/gui/widgets/group.hpp
@@ -0,0 +1,37 @@
+/**
+ *	A group widget implementation
+ *	Nana C++ Library(http://www.nanaro.org)
+ *	Copyright(C) 2015 Jinhao(cnjinhao@hotmail.com)
+ *
+ *	Distributed under the Boost Software License, Version 1.0. 
+ *	(See accompanying file LICENSE_1_0.txt or copy at 
+ *	http://www.boost.org/LICENSE_1_0.txt)
+ *
+ *	@file: nana/gui/widgets/group.hpp
+ *
+ *	@contributors: Stefan Pfeifer (st-321), Jinhao, Ariel Vina-Rodriguez (qPCR4vir)
+ *
+ *	@brief group is a widget used to visually group and layout other widgets.
+ */
+
+#ifndef NANA_GUI_WIDGETS_GROUP_HPP
+#define NANA_GUI_WIDGETS_GROUP_HPP
+
+#include <nana/gui/place.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/panel.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/drawing.hpp>
+
+namespace nana{
+    class group
+	    : public panel<true>
+    {
+
+
+    };
+
+
+
+}//end namespace nana
+#endif
