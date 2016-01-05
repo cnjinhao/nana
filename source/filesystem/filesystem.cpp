70ecce5962a100586000edbf90315e0f8c7e9c71
Jinhao <cnjinhao@hotmail.com>
2016-01-06 00:23:27 +0800

    remove some useless trait classes

--- source/filesystem/filesystem.cpp
+++ source/filesystem/filesystem.cpp
@@ -1,6 +1,6 @@
 /*
  *	A ISO C++ FileSystem Implementation
- *	Copyright(C) 2003-2015 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2003-2016 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0.
  *	(See accompanying file LICENSE_1_0.txt or copy at
