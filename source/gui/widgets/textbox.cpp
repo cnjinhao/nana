818136a18adb9dd7fb37d6bc7bf545bdc2f6c052
Jinhao <cnjinhao@hotmail.com>
2016-01-22 00:55:23 +0800

    remove useless included headers in textbox.cpp

--- source/gui/widgets/textbox.cpp
+++ source/gui/widgets/textbox.cpp
@@ -1,7 +1,7 @@
 /*
  *	A Textbox Implementation
  *	Nana C++ Library(http://www.nanapro.org)
- *	Copyright(C) 2003-2015 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2003-2016 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0.
  *	(See accompanying file LICENSE_1_0.txt or copy at
@@ -14,9 +14,6 @@
 #include <nana/gui/widgets/skeletons/text_editor.hpp>
 #include <stdexcept>
 #include <sstream>
-
-#include <nana/gui/detail/bedrock.hpp>
-#include <nana/gui/detail/inner_fwd_implement.hpp>
 
 namespace nana
 {
