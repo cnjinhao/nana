9f6a3afd77a513f0a3ed1d853aa0ba33b371cc2a
Jinhao <cnjinhao@hotmail.com>
2016-06-24 12:59:28 +0800

    debug travis

--- source/gui/detail/basic_window.cpp
+++ source/gui/detail/basic_window.cpp
@@ -247,7 +247,7 @@ namespace nana
 					if (category::flags::root == category)
 					{
 						std::cout<<"basic_window: delete attr root "<<attribute.root<<std::endl;
-						std::cout<<"basic_window: root.effects size "<<attribute.root->effects_edge_numbus.size()<<std::endl;
+						std::cout<<"basic_window: root.effects size "<<attribute.root->effects_edge_nimbus.size()<<std::endl;
 						delete attribute.root;
 						std::cout<<"    delete successfully"<<std::endl;
 					}
