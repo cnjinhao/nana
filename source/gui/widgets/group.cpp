dd0c75fb0d01567c777629b61a78abc41730eca3
Jinhao <cnjinhao@hotmail.com>
2018-04-01 00:02:24 +0800

    update file information

--- source/gui/widgets/group.cpp
+++ source/gui/widgets/group.cpp
@@ -1,7 +1,7 @@
 /**
  *	A group widget implementation
  *	Nana C++ Library(http://www.nanaro.org)
- *	Copyright(C) 2015-2017 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2015-2018 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0. 
  *	(See accompanying file LICENSE_1_0.txt or copy at 
