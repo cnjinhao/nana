d27bb0a7b32d7d627e40de8bcb26cd07afcb534f
qPCR4vir <arielvina@yahoo.es>
2018-08-23 15:17:51 +0200

    try to add headers to projects

--- include/nana/basic_types.hpp
+++ include/nana/basic_types.hpp
@@ -1,13 +1,13 @@
-/*
+/**
  *	Basic Types definition
  *	Nana C++ Library(http://www.nanapro.org)
- *	Copyright(C) 2003-2017 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2003-2018 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0. 
  *	(See accompanying file LICENSE_1_0.txt or copy at 
  *	http://www.boost.org/LICENSE_1_0.txt)
  *
- *	@file: nana/basic_types.hpp
+ *	@file nana/basic_types.hpp
  */
 
 #ifndef NANA_BASIC_TYPES_HPP
