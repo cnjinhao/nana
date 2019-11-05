25353e4325574cfe23e73379452173d42d84cf46
qPCR4vir <arielvina@yahoo.es>
2019-11-05 19:23:50 +0100

    reorganize priorities

--- build/cmake/select_filesystem.cmake
+++ build/cmake/select_filesystem.cmake
@@ -1,15 +1,13 @@
 # The ISO C++ File System Technical Specification (ISO-TS, or STD) was optional.
 #              http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n4100.pdf
 # It is part of c++17.
-# The library may be not available or working correctly in the std library in use. As a workaround we may try
+# The library may be not available or working correctly in the std library you use. As a workaround we may try
 # to "implement" it (ab)using Boost (almost compatible)
 #              http://www.boost.org/doc/libs/1_60_0/libs/filesystem/doc/index.htm
 # or you can choose to use the (partial, but functional) implementation provided by nana.
 # If you include the file <nana/filesystem/filesystem.hpp> or <nana/filesystem/filesystem_ext.hpp>
-# the selected option will be set by nana into std::filesystem
-# By default Nana will try to use the STD. If STD is not available and NANA_CMAKE_FIND_BOOST_FILESYSTEM
-# is set to ON nana will try to use boost if available. Nana own implementation will be use if none of
-# the previus were selected or available.
+# the selected option will be inlined by nana into std::filesystem
+# By default Nana will try to use the STD. If STD is not available Nana own implementation will be used.
 # You can change that default if you change one of the following
 # (please don't define more than one of the _XX_FORCE options):
 option(NANA_CMAKE_NANA_FILESYSTEM_FORCE "Force nana filesystem over ISO and boost?" OFF)
@@ -18,13 +16,20 @@ option(NANA_CMAKE_BOOST_FILESYSTEM_FORCE "Force use of Boost filesystem if avail
 option(NANA_CMAKE_FIND_BOOST_FILESYSTEM "Search: Is Boost filesystem available?" OFF)
 
 if(NANA_CMAKE_NANA_FILESYSTEM_FORCE)
+    if(NANA_CMAKE_STD_FILESYSTEM_FORCE)
+        message (FATAL_ERROR "Defined NANA_CMAKE_NANA_FILESYSTEM_FORCE and NANA_CMAKE_STD_FILESYSTEM_FORCE")
+    endif()
+    if(NANA_CMAKE_BOOST_FILESYSTEM_FORCE)
+        message (FATAL_ERROR "Defined NANA_CMAKE_NANA_FILESYSTEM_FORCE and NANA_CMAKE_BOOST_FILESYSTEM_FORCE")
+    endif()
+
     target_compile_definitions(nana PUBLIC NANA_FILESYSTEM_FORCE)
 
-elseif(NANA_CMAKE_STD_FILESYSTEM_FORCE)
-    target_compile_definitions(nana PUBLIC STD_FILESYSTEM_FORCE)
-    target_link_libraries     (nana PUBLIC stdc++fs)
-
 elseif(NANA_CMAKE_BOOST_FILESYSTEM_FORCE)
+    if(NANA_CMAKE_STD_FILESYSTEM_FORCE)
+        message (FATAL_ERROR "Defined NANA_CMAKE_BOOST_FILESYSTEM_FORCE and NANA_CMAKE_STD_FILESYSTEM_FORCE")
+    endif()
+
     target_compile_definitions(nana PUBLIC BOOST_FILESYSTEM_FORCE)
     # https://cmake.org/cmake/help/git-master/module/FindBoost.html
     # Implicit dependencies such as Boost::filesystem requiring Boost::system will be automatically detected and satisfied,
@@ -49,8 +54,5 @@ else()
     # todo if not test for boost
     # if not add nana filesystem
 endif()
-
-
-
 
 
