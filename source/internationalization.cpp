6f1d5cba9226a13b66221612c24b9b411761648e
cnjinhao <cnjinhao@hotmail.com>
2022-01-30 04:54:27 +0800

    add description of contribution

--- source/internationalization.cpp
+++ source/internationalization.cpp
@@ -1,13 +1,15 @@
 /*
 *	An Implementation of i18n
 *	Nana C++ Library(http://www.nanapro.org)
-*	Copyright(C) 2003-2020 Jinhao(cnjinhao@hotmail.com)
+*	Copyright(C) 2003-2022 Jinhao(cnjinhao@hotmail.com)
 *
 *	Distributed under the Boost Software License, Version 1.0.
 *	(See accompanying file LICENSE_1_0.txt or copy at
 *	http://www.boost.org/LICENSE_1_0.txt)
 *
 *	@file: nana/internationalization.cpp
+*	@list of contributions:
+*		AStepaniuk, Added support for comment lines in PO files(PR#619)
 */
 
 #include <nana/push_ignore_diagnostic>
