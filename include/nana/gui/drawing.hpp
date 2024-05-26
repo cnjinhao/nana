960dc9b72be3156bf7dad5f923582e8811589f21
qPCR4vir <arielvina@yahoo.es>
2024-05-27 01:21:29 +0200

    docs

--- include/nana/gui/drawing.hpp
+++ include/nana/gui/drawing.hpp
@@ -1,13 +1,13 @@
-/*
+/**
  *	A Drawing Implementation
  *	Nana C++ Library(http://www.nanapro.org)
- *	Copyright(C) 2003-2014 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2003-2024 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0. 
  *	(See accompanying file LICENSE_1_0.txt or copy at 
  *	http://www.boost.org/LICENSE_1_0.txt)
  *
- *	@file: nana/gui/drawing.hpp
+ *	@file nana/gui/drawing.hpp
  */
 #ifndef NANA_GUI_DRAWING_HPP
 #define NANA_GUI_DRAWING_HPP
@@ -15,6 +15,7 @@
 #include <nana/push_ignore_diagnostic>
 #include "widgets/widget.hpp"
 #include "../traits.hpp"
+
 namespace nana
 {
 	/// \brief Draw pictures on a widget by specifying a drawing method that will be employed every time the widget refreshes. 
--- source/gui/drawing.cpp
+++ source/gui/drawing.cpp
@@ -1,13 +1,13 @@
-/*
+/**
  *	A Drawing Implementation
  *	Nana C++ Library(http://www.nanapro.org)
- *	Copyright(C) 2003-2015 Jinhao(cnjinhao@hotmail.com)
+ *	Copyright(C) 2003-2024 Jinhao(cnjinhao@hotmail.com)
  *
  *	Distributed under the Boost Software License, Version 1.0. 
  *	(See accompanying file LICENSE_1_0.txt or copy at 
  *	http://www.boost.org/LICENSE_1_0.txt)
  *
- *	@file: nana/gui/drawing.cpp
+ *	@file nana/gui/drawing.cpp
  */
 
 #include "detail/basic_window.hpp"
